91473711a6365e2915580142afccb8cfdcdeb048
Thomas PIERRAIN <thomas@pierrain.net>
2016-09-03 23:17:51 +0200

    Set full path for CheckedFile.xml so that CI test runner don't fail

--- NFluent.35.Tests/StringRelatedTests.cs
+++ NFluent.35.Tests/StringRelatedTests.cs
@@ -14,6 +14,7 @@
 // // --------------------------------------------------------------------------------------------------------------------
 namespace NFluent.Tests
 {
+    using System;
     using System.IO;
     using System.Text;
     using NFluent.Extensibility;
@@ -668,8 +669,8 @@ namespace NFluent.Tests
         [Test]
         public void LongStringErrorMessageIsProperlyTruncated()
         {
-            var checkString = File.ReadAllText("./CheckedFile.xml", Encoding.UTF8);
-            var expectedString = File.ReadAllText("./ExpectedFile.xml", Encoding.UTF8);
+            var checkString = File.ReadAllText(AppDomain.CurrentDomain.BaseDirectory + "\\CheckedFile.xml", Encoding.UTF8);
+            var expectedString = File.ReadAllText(AppDomain.CurrentDomain.BaseDirectory + "\\CheckedFile.xml", Encoding.UTF8);
 
             Check.ThatCode(() =>
             {
